ad220879fd8dcc75324eafc02f8f96a142d08d3d
jesseokeya <jesseokeya@gmail.com>
2019-03-22 15:55:01 -0400

    changes

--- examples/index.js
+++ examples/index.js
@@ -1,8 +1,10 @@
 const Migrant = require('../index')
 
 const migration = new Migrant({
-    up: '',
-    down: ''
+    up: 'mongodb://workbench:workbench1@ds145093.mlab.com:45093/workbench',
+    down: 'mongodb://codefront:codefront@ds129776.mlab.com:29776/codefront'
 })
 
-migration.migrate()
+migration.collections().then(res => {
+    console.log(res)
+})
