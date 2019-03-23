971c812de6a339c5b707951d65e91ce3c79d1543
jesseokeya <jesseokeya@gmail.com>
2019-03-23 13:19:01 -0400

    changes

--- examples/index.js
+++ examples/index.js
@@ -2,7 +2,7 @@ const Migrant = require('../index')
 
 const migrant = new Migrant({
     up: 'mongodb://workbench:workbench1@ds145093.mlab.com:45093/workbench',
-    down: 'mongodb://codefront:codefront@ds129776.mlab.com:29776/codefront'
+    down: ' mongodb://codefront:codefront123@ds249311.mlab.com:49311/codefront'
 })
 
 migrant.migrate()
