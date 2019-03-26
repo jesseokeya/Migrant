db6319bb1269a9b64f198ecb9409383d32554a84
Jesse Okeya <44819310+jesseokeyahealthbrain@users.noreply.github.com>
2019-03-26 09:53:47 -0400

    Update index.js

--- examples/index.js
+++ examples/index.js
@@ -1,8 +1,8 @@
 const Migrant = require('../index')
 
-const migrant = new Migrant({
+const context = new Migrant({
     up: 'mongodb://workbench:workbench1@ds145093.mlab.com:45093/workbench',
     down: ' mongodb://codefront:codefront123@ds249311.mlab.com:49311/codefront'
 })
 
-migrant.migrate()
+context.migrate()
