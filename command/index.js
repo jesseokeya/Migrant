bc2dc605f52b3ca6993f42d273441fa51cfcdf71
jesseokeya <jesseokeya@gmail.com>
2019-03-28 16:29:01 -0400

    changes

---
+++ command/index.js
@@ -0,0 +1,24 @@
+const {Command, flags} = require('@oclif/command')
+
+class OclifExampleSingleJsCommand extends Command {
+  async run() {
+    const {flags} = this.parse(OclifExampleSingleJsCommand)
+    const name = flags.name || 'world'
+    this.log(`hello ${name} from ./src/index.js`)
+  }
+}
+
+OclifExampleSingleJsCommand.description = `Describe the command here
+...
+Extra documentation goes here
+`
+
+OclifExampleSingleJsCommand.flags = {
+  // add --version flag to show CLI version
+  version: flags.version({char: 'v'}),
+  // add --help flag to show CLI version
+  help: flags.help({char: 'h'}),
+  name: flags.string({char: 'n', description: 'name to print'}),
+}
+
+module.exports = OclifExampleSingleJsCommand
