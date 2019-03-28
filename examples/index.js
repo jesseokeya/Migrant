12d310e160e17bc0861d963f03c66a9942858a96
jesseokeya <jesseokeya@gmail.com>
2019-03-28 16:04:51 -0400

    changes

--- examples/index.js
+++ examples/index.js
@@ -2,9 +2,9 @@ const Migrant = require('../index')
 
 const context = new Migrant({
     /** Database uri your migrating from  **/
-    up: process.env.UP,
+    up: 'mongodb://dafe:dafe12@ds237574.mlab.com:37574/orezime',
     /** Database uri our migrating to **/
-    down: proccess.env.DOWN
+    down: 'mongodb://orezime-admin:orezime12@cluster0-shard-00-00-qpxel.gcp.mongodb.net:27017,cluster0-shard-00-01-qpxel.gcp.mongodb.net:27017,cluster0-shard-00-02-qpxel.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
 })
 
 /** performs database migration **/
