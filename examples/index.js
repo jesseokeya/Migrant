f130b398e2b15e995ac066aac7031b615f4f39be
jesseokeya <jesseokeya@gmail.com>
2019-03-28 16:01:12 -0400

    changes

--- examples/index.js
+++ examples/index.js
@@ -1,11 +1,10 @@
-/** imports the migrant module **/
 const Migrant = require('../index')
 
 const context = new Migrant({
-    /** up is the database uri your migrating from  **/
-    up: 'mongodb://dafe:dafe12@ds237574.mlab.com:37574/orezime',
-    /** down is the database uri our migrating to **/
-    down: 'mongodb://orezime-admin:orezime12@cluster0-shard-00-00-qpxel.gcp.mongodb.net:27017,cluster0-shard-00-01-qpxel.gcp.mongodb.net:27017,cluster0-shard-00-02-qpxel.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+    /** Database uri your migrating from  **/
+    up: process.env.UP,
+    /** Database uri our migrating to **/
+    down: proccess.env.DOWN
 })
 
 /** performs database migration **/
