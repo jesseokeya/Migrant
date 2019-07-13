b8ac095bff014005049a3039be4566cb22a8f22a
jesseokeya <jesseokeya@gmail.com>
2019-07-12 22:58:58 -0400

    Update index.js

--- command/src/index.js
+++ command/src/index.js
@@ -1,11 +1,13 @@
+require("colors");
 const Migrant = require("../../lib");
 const { Command, flags } = require("@oclif/command");
 
 class HealthbrainMigrantCommand extends Command {
   async run() {
     const { flags } = this.parse(HealthbrainMigrantCommand);
-    const name = flags.name || "world";
-    this.log(`hello ${name} from ./src/index.js`);
+    // const name = flags.name || "world"; 
+    this.log('\n                       🚀 ', ` Migrant `.white.bold.inverse, '🚀 \n')
+    this.log(`Perform MongoDb Migrations Easily. Use --help flag to learn more \n`.white.bold.inverse);
   }
 }
 
