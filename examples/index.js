fe01f08c9638e9ce03e2c93e8547012293a10a39
jesseokeya <jesseokeya@gmail.com>
2019-07-12 23:06:09 -0400

    Update index.js

--- examples/index.js
+++ examples/index.js
@@ -2,9 +2,9 @@ const Migrant = require('../index')
 
 const context = new Migrant({
     /** Database uri your migrating from  **/
-    up: 'mongodb://healthbrain:healthbrain@ds255258.mlab.com:55258/healthbraindev',
+    up: process.env.UP,
     /** Database uri our migrating to **/
-    down: 'mongodb+srv://jesseokeya:ov6CCAcEWxpMaXKw@cluster0-gseyx.mongodb.net/test?retryWrites=true&w=majority'
+    down: process.env.DOWN
 })
 
 /** performs database migration **/
