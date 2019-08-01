4b44a7e33bb855fa429c618bf9a7a7369c1ee4f8
jesseokeya <jesseokeya@gmail.com>
2019-08-01 12:02:07 -0400

    changes

--- command/src/index.js
+++ command/src/index.js
@@ -1,28 +1,58 @@
-require("colors");
-const Migrant = require("../../lib");
+const { isEmpty } = require("lodash");
 const { Command, flags } = require("@oclif/command");
+const { description } = require("../util");
+const Migrant = require("../../index");
 
-class HealthbrainMigrantCommand extends Command {
+class MigrantCommand extends Command {
   async run() {
-    // const { flags } = this.parse(HealthbrainMigrantCommand);
-    this.log('\n                       🚀 ', ` Migrant `.white.bold.inverse, '🚀 \n')
-    this.log(`Perform MongoDb Migrations Easily. Use --help flag to learn more \n`.white.bold.inverse);
+    try {
+      const { flags } = this.parse(MigrantCommand);
+      if (isEmpty(flags)) {
+        const message = "invalid command. use the migrant --help flag";
+        this.error(message);
+        this.log(description);
+      } else {
+        const { up, down } = flags;
+        if (!isEmpty(up) && !isEmpty(down)) {
+          const ctx = new Migrant({ up, down });
+          await ctx.migrate();
+        }
+      }
+    } catch (err) {
+      throw err;
+    }
   }
 }
 
-HealthbrainMigrantCommand.description = `Perform mongoDb migrations easily
+MigrantCommand.description = `Perform mongoDb migrations easily
 Make sure your database uri permissions give you read write access
 
 Give us a star on github 
 https://github.com/jesseokeyahealthbrain/Migrant
 `;
 
-HealthbrainMigrantCommand.flags = {
+MigrantCommand.flags = {
   // add --version flag to show CLI version
   version: flags.version({ char: "v" }),
   // add --help flag to show CLI version
   help: flags.help({ char: "h" }),
-  migrate: flags.string({ char: "n", description: "performs database migration from up to down" })
+  up: flags.string({
+    char: "u",
+    description:
+      "specifies the database your migrating from and is dependent on the --down flag",
+    hidden: false,
+    multiple: false,
+    dependsOn: ["down"]
+  }),
+  down: flags.string({
+    char: "d",
+    description:
+      "specifies the database your migrating to and is dependent on the --up flag",
+    multiple: true,
+    hidden: false,
+    multiple: false,
+    dependsOn: ["up"]
+  })
 };
 
-module.exports = HealthbrainMigrantCommand;
+module.exports = MigrantCommand;
---
+++ command/util/index.js
@@ -0,0 +1,20 @@
+const description = `
+Perform mongoDb migrations easily
+
+USAGE
+  $ migrant
+
+OPTIONS
+  -d, --down=down  specifies the database your migrating to and is dependent on the --up flag
+  -h, --help       show CLI help
+  -u, --up=up      specifies the database your migrating from and is dependent on the --down flag
+  -v, --version    show CLI version
+
+DESCRIPTION
+  Make sure your database uri permissions give you read write access
+
+  Give us a star on github 
+  https://github.com/jesseokeyahealthbrain/Migrant
+  `;
+
+  module.exports = { description }
